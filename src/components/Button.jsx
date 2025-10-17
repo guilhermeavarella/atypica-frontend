32a2cc8012bd33b5b88ce0c56553401751196a46
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2025-10-17 11:47:41 -0300

    feat: button component variant

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function Button({ text, onClick }) {
+export default function Button({ text, onClick, variant }) {
+  const baseStyles = "min-h-[2.25rem] py-[0.75rem] rounded-3xl inline-flex justify-center items-center gap-1";
+  const variantStyles = {
+    primary: "bg-brand-primary text-content-inverse font-poppins px-[2rem]",
+    secondary: "bg-0 font-general px-1",
+  };
+
   return (
-    <button className="h-[2.25rem] py-[0.75rem] px-[2rem] bg-brand-primary rounded-3xl inline-flex justify-center items-center gap-1" onClick={onClick}>
-      <div className="text-center justify-center text-content-inverse text-sm font-semibold font-poppins">{text}</div>
+    <button className={`${baseStyles} ${variantStyles[variant]}`} onClick={onClick}>
+      <div className="text-center justify-center text-sm font-semibold">{text}</div>
     </button>
   )
 }
