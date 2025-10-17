eabfa03d0a17f86d492b5111bd3503145b5577c2
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2025-10-16 22:24:20 -0300

    fix: SectionButton style

--- src/components/SectionButton.jsx
+++ src/components/SectionButton.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 export default function Button({ text, onClick, icon }) {
   return (
-    <div className="self-stretch p-3 bg-white rounded-2xl outline outline-1 outline-content-light inline-flex justify-start items-center gap-2 overflow-hidden" onClick={onClick}>
-      <div className="w-6 h-6 relative">
-          <img src={icon} className="w-6 h-6 left-0 top-0 absolute" />
-      </div>
+    <button className="self-stretch p-3 bg-white rounded-2xl outline outline-1 outline-content-light inline-flex justify-start items-center gap-2 overflow-hidden" onClick={onClick}>
+      <img src={icon} className="w-6 h-6" />
       <div className="justify-start text-content-primary text-sm font-normal font-general">{text}</div>
-    </div>
+    </button>
   )
 }
