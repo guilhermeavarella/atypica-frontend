c0593c308bc503b50732f535eb54ed7febbca3c6
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2025-10-17 11:51:54 -0300

    fix: button style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 export default function Button({ text, onClick, variant }) {
-  const baseStyles = "min-h-[2.25rem] py-[0.75rem] rounded-3xl inline-flex justify-center items-center gap-1";
+  const baseStyles = "min-h-[2.25rem] py-[0.5rem] rounded-3xl inline-flex justify-center items-center gap-1";
   const variantStyles = {
-    primary: "bg-brand-primary text-content-inverse font-poppins px-[2rem]",
-    secondary: "bg-0 font-general px-1",
+    primary: "bg-brand-primary text-content-inverse px-[2rem]",
+    secondary: "bg-0 px-1",
   };
 
   return (
     <button className={`${baseStyles} ${variantStyles[variant]}`} onClick={onClick}>
-      <div className="text-center justify-center text-sm font-semibold">{text}</div>
+      <div className="text-center justify-center text-sm font-semibold font-poppins">{text}</div>
     </button>
   )
 }
