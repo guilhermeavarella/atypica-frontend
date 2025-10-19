fdc304c89bf4b54812b9f5985688370314f51ddd
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2025-10-19 12:23:33 -0300

    feat[button]: on hover effect

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export default function Button({ text, onClick, variant }) {
-  const baseStyles = "min-h-[2.25rem] py-[0.5rem] rounded-3xl inline-flex justify-center items-center gap-1";
+export default function Button({ variant, text, onClick}) {
+  const baseStyles = "min-h-[2.25rem] py-[0.5rem] rounded-3xl inline-flex justify-center items-center gap-1 font-semibold";
   const variantStyles = {
-    primary: "bg-brand-primary text-content-inverse px-[2rem]",
-    secondary: "bg-0 px-1",
+    primary: "bg-brand-primary text-content-inverse px-[2rem] hover:bg-brand-primary-light",
+    secondary: "bg-0 px-1 hover:font-bold",
   };
 
   return (
     <button className={`${baseStyles} ${variantStyles[variant]}`} onClick={onClick}>
-      <div className="text-center justify-center text-sm font-semibold font-poppins">{text}</div>
+      <div className="text-center justify-center text-sm font-poppins">{text}</div>
     </button>
   )
 }
